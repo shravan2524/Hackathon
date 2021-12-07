9c7e196bc6ecebd5fb5575b2246687df2ca3a549
shravan2524 <shravanchenna6@gmail.com>
2021-12-07 21:28:40 +0530

    first commt

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,27 @@
-function App() {
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from './Components/Login';
+import SIgnup from './Components/SIgnup';
+import Forget from './Components/Forget';
+import Maindashboard from './Components/Maindashboard';
+import MainProductsPage from './Components/Khyati/MainProductsPage';
+export default function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <>
+      <Router>
+          <Route exact path="/" component={Maindashboard} />
+          <Route exact path="/signup">
+            <SIgnup />
+          </Route>
+          <Route exact path="/mainproducts">
+            <MainProductsPage />
+          </Route>
+      </Router>
+    </>
+  )
 }
-
-export default App;
---
+++ Frontend/src/Components/Maindashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Carousel from './Shravan/Carousel'
+export default function Maindashboard() {
+    return (
+        <>
+            Login
+        </>
+    )
+}
